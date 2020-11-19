62d11a128c9d46c6666c14e70783d29dd862511e
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-11-19 23:51:41 +0700

    add weighted edges, visualize graph

--- testing algorithm.py
+++ testing algorithm.py
@@ -4,7 +4,6 @@ author:
 """
 import matplotlib.pyplot as plt
 import networkx as nx
-import csv
 import pandas as pd
 
 filename = "graph 1.csv"
@@ -15,12 +14,24 @@ G = nx.Graph()
 G.add_nodes_from(data.columns[1:])
 print(G.nodes)
 
+
 for row in data.index.array:
     for col in data.columns[1:]:
         data = data.fillna(0)
         if data.iloc[row][col] != 0:
-            G.add_weighted_edges_from([row, col, data.iloc[row][col]])
+            G.add_edge(data.iloc[row][0], col, weight = data.iloc[row][col])
    
             
 #print(data.iloc[0]['B'])           
 print(G.edges)
+
+pos = nx.spring_layout(G)  # positions for all nodes
+
+print (pos)
+# nodes
+nx.draw_networkx_nodes(G, pos, node_size=700)
+
+# edges
+nx.draw_networkx_edges(G, pos, edgelist=G.edges, width=6)
+plt.axis("off")
+plt.show()
