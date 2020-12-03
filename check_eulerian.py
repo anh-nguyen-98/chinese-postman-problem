58be084315dbb667c9b48566b52f5534e5f42435
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-12-04 00:17:04 +0700

    split check_eulerian into a new file

--- check_eulerian.py
+++ check_eulerian.py
@@ -1,4 +1,7 @@
-
+"""
+Checks whether a graph is Eulerian.
+author: Nguyen Ba Hoc, Nguyen Hoang Nam Anh
+"""
 def is_eulerian (G):
     degree_list = [val for (node, val) in G.degree()]
     for i in degree_list:
