65058966acc168faf19e0c66a4a442fecd96e22c
anh-nguyen-98 <anh.nguyen.190005@student.fulbright.edu.vn>
2020-12-03 19:44:11 +0700

    resolve conflict

--- testing algorithm.py
+++ testing algorithm.py
@@ -1,5 +1,4 @@
 
-
 """
 Discrete math final project
 author: Nguyen Ba Hoc, Nguyen Hoang Nam Anh
@@ -14,10 +13,14 @@ author: Nguyen Ba Hoc, Nguyen Hoang Nam Anh
         length path
         odd degree list
         possible pairing between odd degree vertices
+        Dijkstra
 """
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
+import sys
+
+from collections import defaultdict
 
 filename = "graph 1.csv"
 with open(filename, 'r') as in_file:
@@ -25,14 +28,16 @@ with open(filename, 'r') as in_file:
     
 G = nx.Graph()
 G.add_nodes_from(data.columns[1:])
+print(G.nodes)
 
 for row in data.index.array:
     for col in data.columns[1:]:
         data = data.fillna(0)
         if data.iloc[row][col] != 0:
             G.add_edge(data.iloc[row][0], col, weight = data.iloc[row][col])
-   
-pos = nx.spring_layout(G)  # positions for all nodes
+
+# positions for all nodes
+pos = nx.spring_layout(G)
 
 # nodes
 nx.draw_networkx_nodes(G, pos, node_size=700)
@@ -41,10 +46,11 @@ nx.draw_networkx_nodes(G, pos, node_size=700)
 nx.draw_networkx_edges(G, pos, edgelist=G.edges, width=6)
 plt.axis("off")
 
+#check eulerian
 def is_eulerian (G):
     degree_list = [val for (node, val) in G.degree()]
     for i in degree_list:
-        if i %2 == 1:
+        if i % 2 == 1:
             return False
     return True
         
@@ -54,6 +60,7 @@ def get_length_path (G):
     for edge in G.edges:
         total_weight += G.get_edge_data(edge[0], edge[1])["weight"]
     return total_weight
+print(get_length_path(G))
 
 #step 2: list of odd-deree vertices:
 def get_odd_degree_list(G): 
@@ -65,13 +72,10 @@ def get_odd_degree_list(G):
     return odd_degree_list
 
 #step 3:
-
 #odd_degree_list = (A, B, C, D)
 #=> 3 ways to pair
 #1: A-B, C-D
-
 #2: A-C, B-D
-
 #3: A-D, B-C
 odd_degree_list = get_odd_degree_list(G)
 
@@ -105,6 +109,88 @@ def pairing_vertex(od_list, ret_list, singular_ord):
     
     return ret_list
         
-print(pairing_vertex(odd_degree_list, [], []))
+#print(pairing_vertex(odd_degree_list, [], []))
         
+
+def possible_paths():
+    edges = G.edges()
+    graph = defaultdict(list)
+    #print(edges)
+    for edge in edges:
+        a, b = edge[0], edge[1]
+        graph[a].append(b)
+        graph[b].append(a)
+    return graph
+
+with open(filename, 'r') as in_file:
+    data = pd.read_csv(filename)
+list_of_rows = [list(row)[1:] for row in data.values]
+print(list_of_rows)
+  
+class Graph(): 
+  
+    def __init__(self, vertices): 
+        self.V = vertices 
+        self.graph = [[0 for column in range(vertices)]  
+                    for row in range(vertices)] 
+  
+    def printSolution(self, dist): 
+        print ("Vertex \t Distance from Source")
+        for node in range(self.V): 
+            print (node, "\t", dist[node])
+  
+    # A utility function to find the vertex with  
+    # minimum distance value, from the set of vertices  
+    # not yet included in shortest path tree 
+    def minDistance(self, dist, sptSet): 
+  
+        # Initilaize minimum distance for next node 
+        min = sys.maxsize 
+  
+        # Search not nearest vertex not in the  
+        # shortest path tree 
+        for v in range(self.V): 
+            if dist[v] < min and sptSet[v] == False: 
+                min = dist[v] 
+                min_index = v 
+  
+        return min_index 
+  
+    # Funtion that implements Dijkstra's single source  
+    # shortest path algorithm for a graph represented  
+    # using adjacency matrix representation 
+    def dijkstra(self, src): 
+  
+        dist = [sys.maxsize] * self.V 
+        dist[src] = 0
+        sptSet = [False] * self.V 
+  
+        for cout in range(self.V): 
+  
+            # Pick the minimum distance vertex from  
+            # the set of vertices not yet processed.  
+            # u is always equal to src in first iteration 
+            u = self.minDistance(dist, sptSet) 
+  
+            # Put the minimum distance vertex in the  
+            # shotest path tree 
+            sptSet[u] = True
+  
+            # Update dist value of the adjacent vertices  
+            # of the picked vertex only if the current  
+            # distance is greater than new distance and 
+            # the vertex in not in the shotest path tree 
+            for v in range(self.V): 
+                if self.graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + self.graph[u][v]: 
+                        dist[v] = dist[u] + self.graph[u][v] 
+  
+        self.printSolution(dist) 
+  
+# Driver program 
+g = Graph(5)
+
+g.graph = list_of_rows 
+
+g.dijkstra(3)
+
    
