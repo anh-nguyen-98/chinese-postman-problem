be5d0c64cb267a3a7eafcd8f32e33c57c7116f02
TonyHocNguyen <hoc.nguyen.190028@student.fulbright.edu.vn>
2020-12-08 16:56:41 +0700

    delete some empty lines

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from shortest_pairing import find_shortest_pairing
 """
     Accomplishments so far:
         read csv file for graph data
-        check if eulerian
+        check if Eulerian
         draw graph on networkx
         length path
         odd degree list
@@ -23,44 +23,32 @@ import networkx as nx
 import pandas as pd
 import sys
 
+
 def main():
     filename = "graph 1.csv"
     G = graph.retrieve_graph(filename)
     if not check_eulerian.is_eulerian(G):
-        #odd-degree
+        # odd-degree
         odd_degree_list = get_odd_degree_list(G)
         pairings = pairing_vertex(odd_degree_list, [], [], len(odd_degree_list))
-        print ("Pairings: ")
+        print("Pairings: ")
         print(pairings)
 
         distances = find_shortest_path(filename, G)
-        print ("Shortest distances from each source node to other nodes: ")
-        print (distances)
+        print("Shortest distances from each source node to other nodes: ")
+        print(distances)
 
-        #return id of shortest pairing in pairings
-        shortest_pairing = find_shortest_pairing(G, pairings,distances)
+        # return id of shortest pairing in pairings
+        shortest_pairing = find_shortest_pairing(G, pairings, distances)
 
-        print("\nshortest pairing: " )
+        print("\nshortest pairing: ")
         print(pairings[shortest_pairing])
         G.add_edges_from(pairings[shortest_pairing])
         print(G.edges.data())
     else:
         print(G.edges.data())
 
+    # visualize_graph
 
-
-
-
-
-
-
-
-
-
-    #visualize_graph
 
 main()
-
-
-
-
